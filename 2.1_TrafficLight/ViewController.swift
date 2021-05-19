fb14201935472c7ed62fb3480d62e297e47f2afc
Maria Masljonok <maria@onoffapp.com>
2021-05-19 22:18:46 +0300

    fix crash

--- 2.1_TrafficLight/ViewController.swift
+++ 2.1_TrafficLight/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red,
+         yellow,
+         green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    
+    @IBOutlet var yellowLight: UIView!
+    
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var actionButton: UIButton!
+    
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.layer.cornerRadius = 63
+        yellowLight.layer.cornerRadius = 63
+        greenLight.layer.cornerRadius = 63
+        
+        actionButton.layer.cornerRadius = 10
+        
     }
+    
 
-
+    @IBAction func turnOnTrafficLight() {
+        if actionButton.currentTitle == "START" {
+            actionButton.setTitle("NEXT", for: .normal)
+    }
+   
+        
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenLight.alpha = lightIsOn
+            yellowLight.alpha = lightIsOff
+            currentLight = .red
+        }
+    }
 }
 
